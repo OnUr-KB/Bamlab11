5b1c1e5c8b820b6ffaba25b0da79e61bc3f79aa9
OnUr-KB <kkanya0947@gmail.com>
2023-12-22 15:22:18 +0700

    finish

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,16 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<ctime>
+#include<string>
+using namespace std;
+
+int main(){
+    string Grade[]={ "A", "B+", "B", "C+", "C", "D+", "D", "F" ,"W"};
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    srand(time(0));
+    int x = rand()% 9 ;
+    cout << "You will get " <<Grade[x]<< " in this 261102." ;
+
+}
